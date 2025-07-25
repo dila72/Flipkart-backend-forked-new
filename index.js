fe18570009b22084bb4bf3b169035a3920c41654
nilanga dilhara <nilangadilhara7@gmail.com>
2025-07-25 13:21:50 +0000

    remove firebase

--- index.js
+++ index.js
@@ -37,8 +37,6 @@ app.get("/product/:id", async (req, res) => {
   }
 });
 
-const PORT = process.env.PORT || 3000;
-
-app.listen(PORT, "0.0.0.0", () => {
-  console.log(`Server running on port 3000`);
+app.listen(8080, () => {
+  console.log("Server running on http://localhost:8080");
 });
