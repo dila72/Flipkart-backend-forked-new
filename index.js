c2091fcf0233bc4ed6a4570d32fb4930d04e0d4d
nilanga dilhara <nilangadilhara7@gmail.com>
2025-07-25 11:05:44 +0000

    deployment

--- index.js
+++ index.js
@@ -37,6 +37,8 @@ app.get("/product/:id", async (req, res) => {
   }
 });
 
-app.listen(8080, () => {
-  console.log("Server running on http://localhost:8080");
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server running on port 3000`);
 });
